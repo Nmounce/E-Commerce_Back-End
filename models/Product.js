d5b6f2ff2c8fda67fd33a374f08a429d6a6bd1f7
NikkiMounce <nikkimounce@gmail.com>
2021-09-23 10:02:40 -0700

    defined columns in product.js

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,8 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -7,17 +10,24 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+Product.init({
+  // define columns
+  product_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   }
-);
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+});
 
 module.exports = Product;
