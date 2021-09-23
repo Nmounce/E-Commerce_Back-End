124a3213c7c11b60e6085f25574bf254cd7eca0b
NikkiMounce <nikkimounce@gmail.com>
2021-09-23 11:10:35 -0700

    added associations to index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_pTag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
