1064d707d556cf441dc75e1d18e0e7cc32bf3270
NikkiMounce <nikkimounce@gmail.com>
2021-09-23 21:29:44 -0700

    turning in for the night

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ const {
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // find all products
     // be sure to include its associated Category and Tag data
     try {
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     // find a single product by its `id`
     // be sure to include its associated Category and Tag data
     try {
@@ -74,7 +74,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     // update product data
     Product.update(req.body, {
             where: {
@@ -129,8 +129,8 @@ router.put('/:id', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
-    // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+    // remove one product by its `id` value
     try {
         const productData = await Product.destroy({
             where: {
